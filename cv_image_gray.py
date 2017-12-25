ae07c40e1a5b8146b276cd4dd3f949bd37edc01e
manish2619 <manish1588@gmail.com>
2017-12-25 10:16:08 +0530

    changed cv_image_gray.py

--- cv_image_gray.py
+++ cv_image_gray.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img=cv2.imread('image.jpg',cv2.IMREAD_GRAYSCALE)
-
+#IMREAD_COLOR
+#IMREAD_UNCHANGED
 cv2.imshow('image1',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
